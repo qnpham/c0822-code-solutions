78de0c4d00de24682591d59bdc687c8226e57340
Quan Pham <mockanterr@gmail.com>
2022-08-30 18:09:25 +0000

    added extra info for each order

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,41 +1,71 @@
 var orderHistory = [
-  [{
-    category: 'books',
-    author: 'Faroult, Stephane',
-    brand: null,
-    product: 'The Art of Sql',
-    price: 33.99,
-    order_date: 'July 5, 2020'
-  }, {
-    category: 'eletronics',
-    author: null,
-    brand: 'Nintendo',
-    product: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    price: 94.95,
-    order_date: 'July 5, 2020'
-  }],
-  [{
-    category: 'eletronics',
-    author: null,
-    brand: 'Nintendo',
-    product: 'GameCube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U',
-    price: 15.98,
-    order_date: 'July 4, 2020'
-  }],
-  [{
-    category: 'books',
-    author: 'Alexander, Christopher',
-    brand: null,
-    product: 'The Timeless Way of BUilding',
-    price: 41.33,
-    order_date: 'July 19, 2020'
-  }],
-  [{
-    category: 'books',
-    author: 'Rauschmayer, Dr. Axel',
-    brand: null,
-    product: 'JavaScript for impatient programmers',
-    price: 31.55,
-    order_date: 'August 4, 2020'
-  }]
+  {
+    order: [{
+      category: 'books',
+      author: 'Faroult, Stephane',
+      brand: null,
+      product: 'The Art of Sql',
+      price: 33.99
+    }, {
+      category: 'eletronics',
+      author: null,
+      brand: 'Nintendo',
+      product: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      price: 94.95
+    }],
+    order_id: '#133-2883177-2648248',
+    order_date: 'July 3, 2020',
+    delivery_date: 'July 5, 2020',
+    ship_to: 'JS Masher',
+    total: 138.93,
+    return: 'August 4, 2020'
+  },
+  {
+    order: [{
+      category: 'eletronics',
+      author: null,
+      brand: 'Nintendo',
+      product: 'GameCube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U',
+      price: 15.98
+    }],
+    order_id: '#114-2875557-9059409',
+    order_date: 'July 4, 2020',
+    delivery_date: 'July 7, 2020',
+    ship_to: 'JS Masher',
+    total: 17.22,
+    return: 'July 4, 2020'
+  },
+  {
+    order: [{
+      category: 'books',
+      author: 'Alexander, Christopher',
+      brand: null,
+      product: 'The Timeless Way of Building',
+      price: 41.33
+    }],
+    order_id: '#113-9984268-1280257',
+    order_date: 'July 19, 2020',
+    delivery_date: 'July 20, 2020',
+    ship_to: 'JS Masher',
+    total: 44.53,
+    return: 'July 20, 2020'
+  },
+  {
+    order: [{
+      category: 'books',
+      author: 'Rauschmayer, Dr. Axel',
+      brand: null,
+      product: 'JavaScript for impatient programmers',
+      price: 31.55,
+      order_date: 'August 4, 2020'
+    }],
+    order_id: '#114-3941689-8772232',
+    order_date: 'August 4, 2020',
+    delivery_date: 'August 8, 2020',
+    ship_to: 'JS Masher',
+    total: 34,
+    return: 'August 4, 2020'
+  }
 ];
+
+console.log(orderHistory);
