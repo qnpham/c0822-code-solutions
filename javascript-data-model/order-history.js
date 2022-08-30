de0b57b56f8d7444192ccb0b868317e50be78e2a
Quan Pham <mockanterr@gmail.com>
2022-08-30 17:05:19 +0000

    finished javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,41 @@
+var orderHistory = [
+  [{
+    category: 'books',
+    author: 'Faroult, Stephane',
+    brand: null,
+    product: 'The Art of Sql',
+    price: 33.99,
+    order_date: 'July 5, 2020'
+  }, {
+    category: 'eletronics',
+    author: null,
+    brand: 'Nintendo',
+    product: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    price: 94.95,
+    order_date: 'July 5, 2020'
+  }],
+  [{
+    category: 'eletronics',
+    author: null,
+    brand: 'Nintendo',
+    product: 'GameCube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U',
+    price: 15.98,
+    order_date: 'July 4, 2020'
+  }],
+  [{
+    category: 'books',
+    author: 'Alexander, Christopher',
+    brand: null,
+    product: 'The Timeless Way of BUilding',
+    price: 41.33,
+    order_date: 'July 19, 2020'
+  }],
+  [{
+    category: 'books',
+    author: 'Rauschmayer, Dr. Axel',
+    brand: null,
+    product: 'JavaScript for impatient programmers',
+    price: 31.55,
+    order_date: 'August 4, 2020'
+  }]
+];
