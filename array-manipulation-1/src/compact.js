bbccc020c25b1c051bf7bd1f72c3b8d9e7a6c3d2
Quan Pham <mockanterr@gmail.com>
2022-09-02 19:00:05 +0000

    finished array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,18 @@
 /* exported compact */
+/*
+make an empty result array.
+make a loop, starting from zero and as long as it is less than or equal to parameter length
+minus one, increment by one.
+inside the loop code block, make an if statement.
+if parameter at loop index equals is not falsy, push parameter at loop index into result array.
+after the loop is done, use the push method on result array passing in parameter at loop index.
+*/
+function compact(array) {
+  var result = [];
+  for (var i = 0; i <= array.length - 1; i++) {
+    if (!array[i] === false) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
