d165754f5c6af7b27d880e18e5b22d40bba2a6cb
Quan Pham <mockanterr@gmail.com>
2022-09-06 16:49:49 +0000

    finished dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello, word!');
+console.log('$heading', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+console.log('$explanation', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+console.log('$hint', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+console.log('$p', document.querySelectorAll('p'));
+console.log('$example-link', document.querySelectorAll('.example-link'));
