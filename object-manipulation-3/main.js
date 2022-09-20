41d09e1650a2289c53da518ef8c5aecf674301ce
Quan Pham <mockanterr@gmail.com>
2022-09-20 17:28:24 +0000

    started pseudocode

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,14 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+/*
+make a constructor function for player objects. each having a name and a hand
+property and those are equal to name and hand parameters.
+
+make a giveCard function to loop through suit/ranks based on parameter and return
+a random one only if that card has not been used by any player. if it has, reroll.
+
+make an array to hold all suits.
+make an array to hold all ranks.
+make an array to hold players.
+make all of the player objects with the constructor function, passing in any name
+and giveCall function as second argument and in that function pass in whether.
+*/
