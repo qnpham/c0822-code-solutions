70ac6098d9c28f8397a5bff6c4a846d6d5c7bfb6
Quan Pham <mockanterr@gmail.com>
2022-09-20 02:06:01 +0000

    finished javascript-bank exercise

---
+++ javascript-bank/bank.js
@@ -0,0 +1,34 @@
+/* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && Number.isInteger(balance)) {
+    var account = new Account(this.nextAccountNumber, holder);
+    account.deposit(balance);
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+    return account.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (number === this.accounts[i].number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var result = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    result += this.accounts[i].getBalance();
+  }
+  return result;
+};
