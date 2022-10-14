98e3e1118fd7df4b0f672295a5a7ecd517d4d32b
Quan Pham <mockanterr@gmail.com>
2022-10-14 00:06:45 +0000

    finished express-hello-world exercise

---
+++ express-hello-world/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use((req, res) => {
+  res.send('Hello');
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000');
+});
