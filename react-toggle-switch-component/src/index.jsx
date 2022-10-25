a0a463350202d7f90aa871866f7bbd758423c5d6
Quan Pham <mockanterr@gmail.com>
2022-10-25 21:15:15 +0000

    finished react-toggle-switch-component exercise

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SwitchComponent from './toggle-switch';
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<SwitchComponent />);
