a1b683a303f1254fccdf160ecc76f0748947c1fe
Quan Pham <mockanterr@gmail.com>
2022-10-26 22:52:31 +0000

    made onClick more efficient

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -11,8 +11,10 @@ class Menu extends React.Component {
     this.setState({ viewing: true });
   }
 
-  handleExit() {
-    this.setState({ viewing: false });
+  handleExit(e) {
+    if (e.target.matches('a') || e.target.className.includes('black')) {
+      this.setState({ viewing: false });
+    }
   }
 
   render() {
@@ -39,11 +41,11 @@ class Menu extends React.Component {
       <i className="fa-solid fa-bars" onClick={this.handleEnter}></i>
       <div className='sidebar'>
           <div className="menu-container">
-            <div className="menu onscreen">
+            <div className="menu onscreen" onClick={this.handleExit}>
               <h2>Menu</h2>
-              <a href="#" onClick={this.handleExit}>About</a>
-              <a href="#" onClick={this.handleExit}>Get Started</a>
-              <a href="#" onClick={this.handleExit}>Sign in</a>
+              <a href="#">About</a>
+              <a href="#">Get Started</a>
+              <a href="#">Sign in</a>
             </div>
           </div>
         <div className="black show-black" onClick={this.handleExit}></div>
