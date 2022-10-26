b568e73c6d9a9ab39a8f3786ff60fa8022d0d0b8
Quan Pham <mockanterr@gmail.com>
2022-10-26 16:42:05 +0000

    finished react-rendering-lists exercise

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function ListElements(props) {
+  return <li>{props.name}</li>;
+}
+
+function List(props) {
+  const mapped = props.pokedex.map(e => {
+    return <ListElements key={e.number} name= {e.name} />;
+  });
+  return mapped;
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<List pokedex={pokedex} />);
