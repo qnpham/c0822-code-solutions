4f566b67f5c772512a85a8b1caaef79a82bd8d56
Quan Pham <mockanterr@gmail.com>
2022-10-27 18:19:52 +0000

    changed state model to only have one key

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
 function Elements(props) {
-  const id = props.id;
-
-  if (props.element) {
+  // console.log(props.currentIndex);
+  if (props.currentIndex) {
     return (
-    <div className='topic-container' onClick={e => props.handleTrue(id)}>
+    <div className='topic-container' onClick={() => props.handleTrue(props.id)}>
       <div className="topic-header">
         <li>{props.name}</li>
       </div>
-      <div className="topic-info hidden">
+      <div className="topic-info">
         {props.info}
       </div>
     </div>
     );
   } else {
     return (
-    <div className='topic-container' onClick={e => props.handleFalse(id)}>
+    <div className='topic-container' onClick={() => props.handleFalse(props.id)}>
       <div className="topic-header">
         <li>{props.name}</li>
       </div>
-      <div className="topic-info">
+      <div className="topic-info hidden">
         {props.info}
       </div>
     </div>
@@ -32,9 +31,7 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      1: true,
-      2: true,
-      3: true
+      currentIndex: null
     };
 
     this.handleTrue = this.handleTrue.bind(this);
@@ -42,23 +39,18 @@ class Accordion extends React.Component {
   }
 
   handleTrue(e) {
-
-    for (const key in this.state) {
-      if (Number(key) === e) {
-        this.setState({ [e]: false });
-      } else {
-        this.setState({ [key]: true });
-      }
-    }
+    // console.log('true:', e);
+    this.setState({ currentIndex: true });
   }
 
   handleFalse(e) {
-    this.setState({ [e]: true });
+    // console.log('false:', e);
+    this.setState({ currentIndex: e });
   }
 
   render() {
     const topics = this.props.topics.map(e =>
-      <Elements key={e.id} id={e.id} name={e.name} info={e.info} element={this.state[e.id]} handleTrue={this.handleTrue} handleFalse={this.handleFalse} />
+      <Elements key={e.id} id={e.id} name={e.name} info={e.info} currentIndex={this.state.currentIndex} handleTrue={this.handleTrue} handleFalse={this.handleFalse} />
     );
     return (
       <ul>
